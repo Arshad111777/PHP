c6f889bbf70d7ae008676b70a44796a5fc5823b8
unknown <aliarshad78696@gmail.com>
2021-11-09 08:00:48 +0530

    Initial commit

---
+++ first.php
@@ -0,0 +1,9 @@
+<!-- Display PHP Code  -->
+<?php
+
+echo  "<h1> Hello" . " I Am " . " Arshad Ali" . " </h1> <br/><br/>";
+
+print "<h2><i> Front End Developer </i></h2>";
+
+
+?>
---
+++ variables.php
@@ -0,0 +1,18 @@
+<!-- PHP Variables -->
+
+<?php
+// Define Variables Using $ Sign
+
+$firstName = "Arshad";
+$last_name = "Ali";
+$feel = "How Are You";
+$good = "I Am Fine";
+// Run Code 
+
+echo $firstName . " " .  $last_name. "<br/><br/>";
+print "<strong> Hello My Name Is  " . $firstName  . " " . $last_name . "</strong><br/><br/>";
+
+echo "Question :  "  . $feel . " " . $firstName . " " . $last_name . "<br/>";
+echo "Answer :  "  . "<strong>" . $good . "</strong>"
+
+?>
