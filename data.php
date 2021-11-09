4bd7002b23bcc138f11dcc933287b8b7a23af100
unknown <aliarshad78696@gmail.com>
2021-11-09 16:19:40 +0530

    Initial commit

---
+++ data.php
@@ -0,0 +1,30 @@
+<!-- PHP Variables Data Types  -->
+
+<?php
+
+// String 
+$str = "Hello World";
+// Integer       
+$str = 200;
+// Float              
+$str = 20.50;
+// Boolean
+$str = true;
+// Array
+$str = array("A","B","C");
+// Null
+$str = null;
+echo $str . "<br/>";
+// Var_dump function  Return Value Data Type And Value Character Length & Value   
+ 
+var_dump($str);  
+
+
+
+
+
+var_dump($str)
+
+
+
+?>
